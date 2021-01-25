00147922e41551629a5f66d630f7edb15e3e3163
2dam <markeluralde@gmail.com>
2021-01-25 09:49:38 +0100

    Visitor Domain

--- models/visitor.py
+++ models/visitor.py
@@ -11,28 +11,14 @@ class Visitor (models.Model):
     # Declaracion de los atributos de tipo basico de en Odoo.
     #La clase res.users ya contiene name, email, password, login, login_date (lastConnection)
     dni = fields.Char(string = "DNI")
-    requestedVisitDate = fields.Date(string = "Fecha peticion visita")
+    requestedVisitDate = fields.Date(string = "Fecha peticion visita", domain="[('id','!=',id),('visitDate', '&lt;', requestedVisitDate)]")
     visitReply = fields.Boolean(string = "Respuesta a peticion")
     visited = fields.Boolean(string = "Visitado")
-    visitDate = fields.Date(string = "Fecha visita")
+    visitDate = fields.Date(string = "Fecha visita",  domain="[('id','!=',id),('requestedVisitDate', '&gt;', visitDate)]")
     #Declaracion de los atributos relacionales con otras clase
     #Los visitantes son gestionados por un empleado
     employee = fields.Many2one('emex51_module.employee',ondelete ='set null',string="Empleado")
     #Un visitante visita varios sectores, un sector es visitado por varios  visitantes
     visitedSectors = fields.Many2many('emex51_module.sector',string="Sectores visitados")
-
-    @api.constrains('dni')
-    def dni(self):
-        tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
-        dig_ext = "XYZ"
-        reemp_dig_ext = {'X':'0', 'Y':'1', 'Z':'2'}
-        numeros = "1234567890"
-        dni = dni.upper()
-        if len(dni) == 9:
-            dig_control = dni[8]
-            dni = dni[:8]
-            if dni[0] in dig_ext:
-                dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
-            return len(dni) == len([n for n in dni if n in numeros]) \
-                and tabla[int(dni)%23] == dig_control
-        return False
+    
+    
